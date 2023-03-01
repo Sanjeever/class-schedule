f09bf5ff6780a429c46f74727826166d79af47d5
Sanjeever <78210374+Sanjeever@users.noreply.github.com>
2023-03-01 16:11:51 +0800

    feat(app): app will be mounted after a random millisecond

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./assets/main.css";
+import { getRandomIntInclusive } from "./utils/math";
 
 const app = createApp(App);
 app.directive("focus", {
   mounted: (el) => el.focus(),
 });
-setTimeout(() => app.mount("#app"), 1000);
+setTimeout(() => app.mount("#app"), getRandomIntInclusive(500, 1500));
